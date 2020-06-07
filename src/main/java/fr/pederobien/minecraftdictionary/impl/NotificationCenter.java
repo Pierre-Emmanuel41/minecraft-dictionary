8b56217929ed806d7d8a829b3fc30f61f942231c
Pierre-Emmanuel <pierre.emmanuel41@gmail.com>
2020-06-07 16:30:11 +0200

    Updating project du to update in project fr.pederobien.dictionary

--- src/main/java/fr/pederobien/minecraftdictionary/impl/NotificationCenter.java
+++ src/main/java/fr/pederobien/minecraftdictionary/impl/NotificationCenter.java
@@ -4,9 +4,8 @@ import java.util.stream.Stream;
 
 import org.bukkit.entity.Player;
 
-import fr.pederobien.dictionary.impl.DictionaryManager;
+import fr.pederobien.dictionary.impl.DictionaryContext;
 import fr.pederobien.dictionary.interfaces.IDictionaryContext;
-import fr.pederobien.dictionary.interfaces.IDictionaryManager;
 import fr.pederobien.minecraftdictionary.interfaces.IMinecraftMessageEvent;
 import fr.pederobien.minecraftdictionary.interfaces.IMinecraftNotificationCenter;
 import fr.pederobien.minecraftmanagers.BukkitManager;
@@ -14,10 +13,10 @@ import fr.pederobien.minecraftmanagers.MessageManager;
 import fr.pederobien.minecraftmanagers.PlayerManager;
 
 public class NotificationCenter implements IMinecraftNotificationCenter {
-	private IDictionaryManager dictionaryManager;
+	private IDictionaryContext dictionaryContext;
 
 	private NotificationCenter() {
-		dictionaryManager = new DictionaryManager();
+		dictionaryContext = new DictionaryContext();
 	}
 
 	public static IMinecraftNotificationCenter getInstance() {
@@ -26,6 +25,11 @@ public class NotificationCenter implements IMinecraftNotificationCenter {
 
 	private static class SingletonHolder {
 		public static final IMinecraftNotificationCenter CENTER = new NotificationCenter();
+	}
+
+	@Override
+	public IDictionaryContext getDictionaryContext() {
+		return dictionaryContext;
 	}
 
 	@Override
@@ -44,15 +48,15 @@ public class NotificationCenter implements IMinecraftNotificationCenter {
 	}
 
 	@Override
-	public IDictionaryContext getDictionaryContext() {
-		return dictionaryManager;
+	public String getMessage(IMinecraftMessageEvent event) {
+		return getDictionaryContext().getMessage(event);
 	}
 
 	private void sendMessage(Player player, IMinecraftMessageEvent event) {
 		if (player.equals(event.getPlayer()))
-			MessageManager.sendMessage(player, dictionaryManager.getMessage(event));
+			MessageManager.sendMessage(player, getMessage(event));
 		else
-			MessageManager.sendMessage(player, dictionaryManager.getMessage(new MinecraftMessageEvent(player, event.getCode(), event.getArgs())));
+			MessageManager.sendMessage(player, getMessage(new MinecraftMessageEvent(player, event.getCode(), event.getArgs())));
 	}
 
 	private void sendMessage(Stream<Player> players, IMinecraftMessageEvent event) {
